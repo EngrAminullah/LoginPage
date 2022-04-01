6f40454f9877ff95cd52190afdd6bd028d0c875f
Muhammad Arsalan <amin93pk@gmail.com>
2022-04-01 10:54:02 +0500

    setting

--- Screens/BottomNav.js
+++ Screens/BottomNav.js
@@ -124,14 +124,6 @@ export default function BottomNav() {
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
-            if (route.name === "Home") {
-              iconName = focused
-                ? "ios-information-circle"
-                : "ios-information-circle-outline";
-            } else if (route.name === "Settings") {
-              iconName = focused ? "ios-list-box" : "ios-list";
-            }
-
             // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
